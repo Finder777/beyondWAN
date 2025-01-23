293e6dd5e93a5430822085d289a52aeb590e7794
steadyEddie <ed.ag.ramirez@hotmail.com>
2025-01-23 17:29:37 +0000

    new stuff

--- src/homepage/main.js
+++ src/homepage/main.js
@@ -1,6 +1,7 @@
 // Path to the HTML file: /home/otacon/Dev/beyondWAN/src/index.html
 // Path to the CSS file: /home/otacon/Dev/beyondWAN/src/styles.css
 
+// This is the JS for the hamburger menu
 const hamburgerBtn = document.getElementById('hamburger');
 const navLinks = document.querySelector('.nav-links');
 
@@ -8,14 +9,33 @@ hamburgerBtn.addEventListener('click', () => {
   navLinks.classList.toggle('show'); 
 });
 
-// Get current date and time
-// const now = new Date();
-// console.log(now); 
+const modal = document.getElementById('modal');
+const closeBtn = document.getElementById('close-btn');
+
+hamburgerBtn.addEventListener('click', () => {
+  modal.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
+window.addEventListener('click', (event) => {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+});
+
+// This JS is for everything else like... 
+
+//Get current date and time
+const now = new Date();
+console.log(now); 
 
 // Get the current date
-const now = new Date();
+const justnow = new Date();
 const dateElement = document.getElementById('current-date');
-dateElement.textContent = now.toDateString(); // Set the date to the HTML element
+dateElement.textContent = justnow.toDateString(); // Set the date to the HTML element
 
 // Get user's location (if permitted)
 navigator.geolocation.getCurrentPosition(
