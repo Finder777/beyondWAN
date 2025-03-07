5b050c04f772d3e4a5da95593d145270aa6522bd
steady eddie <ed.ag.ramirez@hotmail.com>
2025-03-07 22:06:00 +0000

    fixes

--- src/hamburger.js
+++ src/hamburger.js
@@ -1,8 +1,15 @@
-//This is what it is
-
 const hamburgerBtn = document.getElementById('hamburger');
 const navLinks = document.querySelector('.nav-links');
+const modal = document.getElementById('modal'); 
 
 hamburgerBtn.addEventListener('click', () => {
   navLinks.classList.toggle('show'); 
+  modal.style.display = modal.style.display === 'block' ? 'none' : 'block';
+});
+
+const closeBtn = document.getElementById('close-btn');
+
+closeBtn.addEventListener('click', ()=>{
+    modal.style.display = 'none';
+    navLinks.classList.remove('show');
 });
